693a8548969f5457673cbfa660f9c1a9646bbaf1
Yuragranovsky <yuragranovsky@gmail.com>
2023-11-19 15:34:22 +0200

    adds MyDictionary task

---
+++ src/telran/interview/MyDictionary.java
@@ -0,0 +1,29 @@
+package telran.interview;
+
+import java.util.List;
+
+public class MyDictionary {
+	//TODO
+	public MyDictionary(List<String> words) {
+		//TODO
+		//Filling words of the dictionary from the given list
+	}
+	/**
+	 * adds word into dictionary
+	 * @param word
+	 * @return true if new word has been added, false if such word already exists
+	 * 
+	 */
+	public boolean addWord(String word) {
+		//TODO
+		return false;
+	}
+	/**
+	 * looking for the words with a given prefix
+	 * @param prefix
+	 * @return array of words with the given prefix
+	 */
+	public String [] getWordsByPrefix(String prefix) {
+		return null;
+	}
+}
